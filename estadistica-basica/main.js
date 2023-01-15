88b2101ea5c6abcc90bcfc8c56f78dee930714ba
Ed Oliveros <edgarolimoy98@gmail.com>
2023-01-14 22:10:44 -0500

    reto terminado, mediana de lista par

--- estadistica-basica/main.js
+++ estadistica-basica/main.js
@@ -41,3 +41,38 @@ function esPar(lista) {
 
     return !(lista.length % 2)
 }
+
+function esImpar(lista) {
+    return (lista.length % 2)
+}
+
+function calcularMediana(array) {
+    const listaEsPar = esPar(array);
+
+
+    if (listaEsPar) {
+
+        const IndexMitadListaPar1 = (array.length / 2) - 1;
+        const IndexMitadListaPar2 = (array.length / 2) ;
+
+        const numero1 = array[IndexMitadListaPar1];
+        const numero2 = array[IndexMitadListaPar2];
+
+        console.log(IndexMitadListaPar1)
+        console.log(IndexMitadListaPar2)
+
+        console.log(numero1, numero2);
+        
+
+    } else {
+        const IndexMitadListaImpar = Math.floor(array.length / 2);
+        const medianaListaImpar = array[IndexMitadListaImpar];
+        console.log(IndexMitadListaImpar);
+        console.log(medianaListaImpar);
+
+        return medianaListaImpar;
+
+        //metodo Math.floor, quita los decimales
+        //Math.floor(10 / 2)
+    }
+}
